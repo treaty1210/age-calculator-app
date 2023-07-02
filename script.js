455b35007a5e0a110cf2e4f6858c8e7c2585e614
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-01 21:32:05 -0700

    Added checkMonth and checkYear
    checkYear will also make check against current year, to prevent
    future years from being valid.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Query Selectors
 let day = document.querySelector("#insertDay");
-let month = document.querySelector("#insertMonth").value;
-let year = document.querySelector("#insertYear").value;
+let month = document.querySelector("#insertMonth");
+let year = document.querySelector("#insertYear");
 let valDay = document.querySelector(".valDay");
 
 
@@ -10,15 +10,17 @@ let valDay = document.querySelector(".valDay");
 // function dayValue(e) {
 //     checkDay()
 //     if (validDay == true) {
-//         valDay.textContent = e.target.value
+//         valDay.textContent = e.target.value //this would replace the information in valDay span
 //     } else {
 //         return false; 
 //     }
 // }
 
-let validDay = false;
+
 
 //check Day
+let validDay = false;
+
 day.addEventListener("input", checkDay)
 
 function checkDay(e) {
@@ -26,34 +28,48 @@ function checkDay(e) {
     console.log(dayValue)
 
     if (dayValue < 1 || dayValue > 31) {
-        console.log("invalid day")
         validDay = false; 
         //make this change box to red for error with error message, will probably have to add a div below and insert text
     } else {
-        console.log("valid day")
         validDay = true;
         //this will have to remove any text in that div if error ran first
     }
+    console.log(validDay)
 }
 
 //check Month
-const validMonth = () => {
-    if (month < 1 || month > 12) {
-        console.log("invalid month")
-        return false;
+let validMonth = false;
+
+month.addEventListener("input", checkMonth)
+
+function checkMonth(e) {
+    let monthValue = e.target.value
+    console.log(monthValue)
+
+    if (monthValue < 1 || monthValue > 12) {
+        validMonth = false;
     } else {
-        console.log("valid month")
-        return true;
+        validMonth = true;
     }
+    console.log(validMonth)
 }
 
 //check Year
-const validYear = () => {
-    if (year < 1) {
-        console.log("invalid year")
-        return false;
+let validYear = false;
+
+year.addEventListener("input", checkYear)
+
+function checkYear(e) {
+    let yearValue = e.target.value
+    console.log(yearValue)
+
+    let currentYear = new Date().getFullYear();
+    console.log(currentYear)
+
+    if (yearValue < 1 || yearValue > currentYear) { //pull current year to check
+        validYear = false;
     } else {
-        console.log("valid year")
-        return true;
-    }
+        validYear = true;
+    } 
+    console.log(validYear)
 }
